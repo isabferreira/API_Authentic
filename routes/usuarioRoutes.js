893d3a895997b61607d135d90ead50ed3d4dde78
Isabela Ferreira <88989947+isabferreira@users.noreply.github.com>
2025-06-01 18:32:29 -0300

    s no midd

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/usuarioController');
-const authMiddleware = require('../middlewares/authMiddleware');
+const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/registrar', controller.registrar);
 router.post('/login', controller.login);
