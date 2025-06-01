a59fa1d68cd8718af7a542d47629a7822b387b79
Isabela Ferreira <88989947+isabferreira@users.noreply.github.com>
2025-06-01 18:29:42 -0300

    rotas protegidas

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/usuarioController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/registrar', controller.registrar);
 router.post('/login', controller.login);
+
 router.put('/usuarios/atualizar', authMiddleware, controller.atualizar);
 router.delete('/usuarios/deletar', authMiddleware, controller.deletar);
 router.get('/usuarios/dados', authMiddleware, controller.buscarDados);
